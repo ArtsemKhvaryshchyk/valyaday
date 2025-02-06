e7da3b9da533186c192864e581ab7263606b5319
Art <129563430+ArtsemKhvaryshchyk@users.noreply.github.com>
2025-02-06 23:46:42 +0100

    Add files via upload

---
+++ yes_script.js
@@ -0,0 +1,51 @@
+function createHearts() {
+    const container = document.body;
+
+    for (let i = 0; i < 20; i++) {
+        const heart = document.createElement('div');
+        heart.classList.add('heart');
+        heart.style.left = `${Math.random() * 100}vw`;
+        heart.style.top = `${Math.random() * 100}vh`;
+        heart.style.animationDuration = `${Math.random() * 3 + 2}s`; 
+        container.appendChild(heart);
+    }
+}
+
+createHearts();
+
+
+const audio = document.getElementById('background-music');
+audio.volume = 0.03; 
+
+
+audio.play().then(() => {
+    console.log("Музыка начала играть автоматически.");
+}).catch((error) => {
+    console.log("Автовоспроизведение заблокировано. Ждем взаимодействия пользователя.");
+    
+    document.addEventListener('click', () => {
+        audio.play();
+    });
+});
+
+
+var duration = 15 * 1000; 
+var animationEnd = Date.now() + duration;
+var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+var interval = setInterval(function() {
+  var timeLeft = animationEnd - Date.now();
+
+  if (timeLeft <= 0) {
+    return clearInterval(interval);
+  }
+
+  var particleCount = 50 * (timeLeft / duration);
+  
+  confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } });
+  confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } });
+}, 250);
